c68ab382b30fbaf81a396396b5e15c09655e8825
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2019-09-30 22:17:40 -0400

    Add 64bit binary capability

--- External-Tools/main.sh
+++ External-Tools/main.sh
@@ -1,5 +1,9 @@
 # External Tools
 
 chmod -R 0755 $TMPDIR/addon/External-Tools
+if $IS64BIT; then
+  [ -d $TMPDIR/addon/External-Tools/tools/arm64 ] && mv -f $TMPDIR/addon/External-Tools/tools/arm64/* $TMPDIR/addon/External-Tools/tools/arm
+  [ -d $TMPDIR/addon/External-Tools/tools/x64 ] && mv -f $TMPDIR/addon/External-Tools/tools/x64/* $TMPDIR/addon/External-Tools/tools/x86
+fi
 cp -R $TMPDIR/addon/External-Tools/tools $UF 2>/dev/null
 [ -d "$UF/tools/other" ] && PATH=$UF/tools/other:$PATH
